adc262c73b6caccea46774a58eca036989b98a5f
Harish-Guwa-au50 <111633392+Harish-Guwa-au50@users.noreply.github.com>
2023-05-22 20:38:12 +0530

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import './landing.css'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Landingpage = () => {
-    // const history = useHistory()
-    // const login=()=>{
-    //     axios.post('http://localhost:8000/landingpage')
-    //     .then(res=>{
-    //         alert(res.data.message)
-    //         // history.push('/h')
-    //     })
-    // }
+    const history = useHistory()
+    const login=()=>{
+        axios.post('http://localhost:8000/landingpage')
+        .then(res=>{
+            alert(res.data.message)
+            // history.push('/h')
+        })
+    }
 
     return (
+  
 
-        <div>
-            <div className="button" onClick={() => history.push('/Buyerregister')} > BUYER</div>
-            <div className="button" onClick={() => history.push('/Sellerregister')} > SELLER</div>
+        <div className='container'>
+            <h1> Are You A </h1>
+          
+            <div className="c1" onClick={() => history.push('/Buyerregister')} > BUYER</div>
+         
+            <div className="c2" onClick={() => history.push('/Sellerregister')} > SELLER</div>
         </div>
 
     )
